bfeb2c27e5549f1575b7de95d8c5765b8e0163a9
Rafael Vareto <rafaelvareto@gmail.com>
2016-04-21 10:55:06 -0300

    Scripts to download dataset

---
+++ FaceScrubSplitter/Script/script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+IFS=$'\r\n' GLOBIGNORE='*' command eval  'LINES=($(cat $1))'
+echo "${LINES[0]}"
+
+
+for L in "${LINES[@]}"
+do
+	IFS='	' read -a words <<< "${L}"
+	
+	nm="${words[0]}"
+	sp="/"
+	id="${words[2]}"
+	ul="${words[3]}"
+	ex=".jpg"
+
+	mkdir $nm
+	sv=$nm$sp$id$ex
+
+	wget -nc $ul --output-document=$sv --tries=1
+done
+
