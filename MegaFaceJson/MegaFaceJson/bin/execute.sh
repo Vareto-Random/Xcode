66a48766227ee1daadc3d27b6719d13c4ea0d6fe
Rafael H. Vareto <rafaelvareto@gmail.com>
2016-05-09 07:25:26 -0300

    Changing execute.sh file

--- MegaFaceJson/MegaFaceJson/bin/execute.sh
+++ MegaFaceJson/MegaFaceJson/bin/execute.sh
@@ -12,5 +12,7 @@
 
 ./MegaFaceJson.app ~/STORAGE/datasets/DATASETS/megaface/FlickrFinal2/ fileList.txt 10000 1
 ./MegaFaceJson.app ~/STORAGE/datasets/DATASETS/megaface/FlickrFinal2/ fileList.txt 50000 1
-./MegaFaceJson.app ~/STORAGE/datasets/DATASETS/megaface/FlickrFinal2/ fileList.txt 100000 1
+./MegaFaceJson.app ~/STORAGE/datasets/DATASETS/megaface/FlickrFinal2/ fileList.txt 100000 1	
 ./MegaFaceJson.app ~/STORAGE/datasets/DATASETS/megaface/FlickrFinal2/ fileList.txt 500000 1
+
+./MegaFaceJson.app ~/STORAGE/datasets/DATASETS/megaface/FlickrFinal2/ fileList.txt 1000000 1
